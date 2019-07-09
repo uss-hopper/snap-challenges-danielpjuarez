9123b1d6a0cbdc2efc948785b52ec6fe1b396a1e
Daniel Juarez <djuarez11@cnm.edu>
2019-07-09 08:41:46 -0600

    Simple function strings for multiplying (function multiply) and concatenating (function puttogther) values

---
+++ snap_7_9/snap7_9.js
@@ -0,0 +1,11 @@
+function multiply(val1,val2) {
+	return val1*val2;
+}
+
+multiply(2,4);
+
+function puttogether(firststring,secondstring) {
+	return (firststring+secondstring);
+}
+
+puttogether ("hello"," hi");
