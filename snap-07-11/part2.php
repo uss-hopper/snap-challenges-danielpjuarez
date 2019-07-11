c2666c293e187b27644eebc36ac788bb22dd69e1
Daniel Juarez <djuarez11@cnm.edu>
2019-07-11 08:29:15 -0600

    Inserted console.log function so program can be run without being placed into browser console

---
+++ snap-07-11/part2.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<!--	Create an ERD for this user story describing an action on an imaginary ecommerce site. -->
+<!--	"As a registered user, I can add multiple shipping addresses to my profile." -->
+<!--	Indicate which entities are strong, and which entities are weak. -->
+<!--	Identify at least two attributes for each, including any primary or foreign keys.-->
+	<head>
+		<meta charset="UTF-8">
+		<title>Part 2</title>
+	</head>
+	<body>
+	<h1>Part 2</h1>
+	<a href="part2.php"> Back to Part 1</a>
+	</body>
+</html>
--- snap_7_9/snap7_9.js
+++ snap_7_9/snap7_9.js
@@ -1,3 +1,11 @@
+// Write two functions in javascript. Make sure that you put them in your GitHub Classroom account in the Snap Challenges project.  Put them in a folder called "snap_7_9"
+//
+// Function #1
+// Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.)  Which operation is your choice.
+//
+// 	Function #2
+// Take two strings as arguments and return a value that has them concatenated.
+
 function multiply(val1,val2) {
 	return val1*val2;
 }
@@ -7,9 +15,17 @@ multiply(2,4);
 console.log(multiply (2,4));
 
 function puttogether(firststring,secondstring) {
-	return (firststring+" "+secondstring);
+
+	return puttogether=(firststring+" "+secondstring);
 }
 
 puttogether ("a","b");
 
 console.log (puttogether ("a","b"));
+
+function add(val1,val2) {
+	return val1+val2
+}
+
+console.log (add (2,4));
+console.log (add ("a", " b"));
