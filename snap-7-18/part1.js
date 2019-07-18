ca317b25a9ec67b3bdb1121abfbd4e2d71441beb
Daniel Juarez <djuarez11@cnm.edu>
2019-07-18 09:34:23 -0600

    forgot to push until now. part2.js is my initial attempt

---
+++ snap-7-18/part1.js
@@ -0,0 +1,24 @@
+/**convert weight or mass to grams
+ *given weightLb set wegithgram to weightLbs/2.2046
+ *@param mass Te weight or mass to be converted
+ * @param units The abbreviation for the units specified in the mass
+ */
+
+function converttograms (mass, units) {
+	$convertedValue = 0;
+
+	if (units==='g'){
+		return mass;
+	} else if (units=== 'lbs') {
+		return mass * 453.592;
+	} else if (units === 'oz') {
+		return mass *28.34952
+	} else if (units === 'kg') {
+		return mass*1000
+	}else if (units === 'mg') {
+		return mass/1000
+	}
+	return $convertedValue;
+}
+
+console.log(converttograms(30,'kg'));
---
+++ snap-7-18/part2.js
@@ -0,0 +1,7 @@
+//loop will find all uniques factors of a given number and sum them.
+// loop will subtract 1 from j each time it runs and divide 24 by the new value of j,
+// and add that value, if it is an integer, to other values that are integers
+
+var j;
+foreach (j=24, j>0, j--); ((sum (output= 24/j)
+if (output===int);
