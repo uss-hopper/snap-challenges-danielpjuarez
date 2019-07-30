aa623013e34c41c5fb9863e24072b0a06c872181
Daniel Juarez <djuarez11@cnm.edu>
2019-07-30 08:20:15 -0600

    initial commit for 7-30 snaps challenege

---
+++ snap-7-30/getTweetsbyTweetDate.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *Using the Tweet.php class found on the Object Oriented PHP page, write and document a getTweetsByTweetDate method
+that takes a DateTime object as a parameter and returns an array of all Tweets on that date.
+link to OOP Tweet example: https://bootcamp-coders.cnm.edu/class-materials/object-oriented/object-oriented-php.php
+Message #web-captains-log
+ */
+
+
+/**
+*gets Tweets by TweetDate
+ *
+ * @param string $tweetContent tweet content to search for
+*@return \SplFixedArray SplFixedArray of Tweets found
+*@throws \PDOexception when mySQL related error occur
+*@throws \TypeError when variables are not the correct data type
+*/
+
