f34a31ad52d177a02704f0d2d96502b128bea775
Daniel Juarez <djuarez11@cnm.edu>
2019-08-20 09:00:28 -0600

    I got somethign working but it only returns uppercase e

--- snap-8-20/8-20.js
+++ snap-8-20/8-20.js
@@ -1,9 +1,12 @@
-let nonsenseStarTrekBabble= ["Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence."];
+let nonsenseStarTrekBabble = "Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.";
 
-let foo= Array.from("Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.");
+let nonSenseStarTrekBabbleArray = nonsenseStarTrekBabble.split(" ");
 
-console.log(foo);
+// console.log(nonSenseStarTrekBabbleArray);
 
-let startsWithE = nonsenseStarTrekBabble.filter((nonsenseStarTrek) => nonsenseStarTrek.startsWith("e"));
+let startsWithE = nonSenseStarTrekBabbleArray.filter((nonsenseStarTrek) => nonsenseStarTrek.startsWith("e"));
 
 console.log(startsWithE);
+
+
+
